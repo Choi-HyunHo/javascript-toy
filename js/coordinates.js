26c32d0feed657952acf0eab78e6b664f0e63b11
com <x7788@naver.com>
2022-01-18 15:14:10 +0900

    좌표 찾기 완성

--- js/coordinates.js
+++ js/coordinates.js
@@ -1,12 +1,17 @@
 const target = document.querySelector('.target')
-const message = document.querySelector('.target-desc')
+const tag = document.querySelector('.tag')
+const horizontal = document.querySelector('.horizontal')
+const vertical = document.querySelector('.vertical')
 
 document.addEventListener('mousemove', (e) => {
   const x = e.clientX
   const y = e.clientY
 
-  target.style.left = x + 'px'
-  target.style.top = y + 'px'
-
-  message.innerHTML = `x: ${x}, y: ${y}`
+  vertical.style.left = `${x}px`
+  horizontal.style.top = `${y}px`
+  target.style.left = `${x}px`
+  target.style.top = `${y}px`
+  tag.style.left = `${x}px`
+  tag.style.top = `${y}px`
+  tag.innerHTML = `${x}px, ${y}px`
 })
