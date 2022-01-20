f5225558b260d1533d3d06fac0476f0f80f9655d
com <x7788@naver.com>
2022-01-20 15:56:40 +0900

    리스트 내용 및 버튼 출력 성공

--- js/shop.js
+++ js/shop.js
@@ -1,11 +1,7 @@
 const inputForm = document.querySelector('.input__form')
 const inputText = document.querySelector('.input__text')
-const hidden = document.querySelector('.hidden')
-const listBox = document.querySelector('.list__item-info')
-const ul = document.querySelector('.list')
 const li = document.querySelector('.list__item')
 
-// Form의 새로고침 막기
 function listAdd(event) {
   event.preventDefault()
   const listItem = inputText.value
@@ -15,9 +11,11 @@ function listAdd(event) {
     div.setAttribute('class', 'list__item-info')
     div.textContent = listItem
     li.append(div)
+
     const btn = document.createElement('button')
     btn.setAttribute('class', 'delete__btn')
-    btn.textContent = div.append(btn)
+    btn.textContent = '❌'
+    div.appendChild(btn)
   }
 }
 
